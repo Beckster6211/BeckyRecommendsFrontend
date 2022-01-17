9041695f634182d361bfa8df43b82a63fc5016dc
= <beckyljones151192@gmail.com>
2022-01-17 15:58:10 +0000

    changed footer slightly

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -3,32 +3,51 @@ import React from "react";
 function Footer() {
   return (
     <div>
-      <footer className="footer">
-        <img
-          className="footerPics"
-          src="./Capture.PNG"
-          alt="SoC avatar"
-          //   onClick={() => loginWithRedirect()}
-          width="100px"
-        />
-        <a
-          className="footerText"
-          href="https://blj-recommendtobecky.netlify.app/"
-        >
-          Recommend to <br /> Becky
-        </a>
-        {/* <h3 className="headerText">
+      <div>
+        <footer className="footer">
+          <img
+            className="footerPics"
+            src="./Capture.PNG"
+            alt="SoC avatar"
+            //   onClick={() => loginWithRedirect()}
+            width="100px"
+          />
+          <a
+            className="footerText"
+            href="https://blj-recommendtobecky.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Recommend to Becky
+          </a>
+          {/* <h3 className="headerText">
           Becky <br /> Recommends
         </h3> */}
 
-        <img
-          className="footerPics"
-          src="./Capture1.PNG"
-          alt="Quackers head"
-          //   onClick={() => logout({ returnTo: window.location.origin })}
-          width="100px"
-        />
-      </footer>
+          <img
+            className="footerPics"
+            src="./Capture1.PNG"
+            alt="Quackers head"
+            //   onClick={() => logout({ returnTo: window.location.origin })}
+            width="100px"
+          />
+        </footer>
+      </div>
+      {/* <div>
+        <a
+          href="https://blj-recommendtobecky.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          <img
+            src="./rectobecpic.PNG"
+            height="150px"
+            width="100%"
+            alt="recbecpic"
+          />
+        </a>
+      </div> */}
     </div>
   );
 }
